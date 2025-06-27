e0d242b04864e6f3a8645035f49da46d88384957
Oliver Popadich <opopadich@squareup.com>
2025-06-27 11:45:30 -0500

    ensure there are no breaks in the clickable area

--- ui/desktop/src/components/ToolCallArguments.tsx
+++ ui/desktop/src/components/ToolCallArguments.tsx
@@ -39,24 +39,28 @@ export function ToolCallArguments({ args }: ToolCallArgumentsProps) {
 
       return (
         <div className="text-sm mb-2">
-          <div className="flex flex-row items-start">
+          <div className="flex flex-row items-stretch">
             <button
               onClick={() => toggleKey(key)}
-              className="text-left text-textSubtle min-w-[140px]"
+              className="flex text-left text-textSubtle min-w-[140px]"
             >
-              {key}
+              <span>{key}</span>
             </button>
-            <div className="w-full flex justify-between items-start">
+            <div className="w-full flex items-stretch">
               {isExpanded ? (
-                <div className="">
+                <div>
                   <MarkdownContent content={value} className="text-sm text-textPlaceholder" />
                 </div>
               ) : (
-                <button onClick={() => toggleKey(key)} className="text-textPlaceholder mr-2">
+                <button onClick={() => toggleKey(key)} className="text-left text-textPlaceholder">
                   {value.slice(0, 60)}...
                 </button>
               )}
-              <button onClick={() => toggleKey(key)} className="text-textPlaceholder pr-2">
+              <button
+                onClick={() => toggleKey(key)}
+                className="flex flex-row items-stretch grow text-textPlaceholder pr-2"
+              >
+                <div className="min-w-2 grow" />
                 <Expand size={5} isExpanded={isExpanded} />
               </button>
             </div>
